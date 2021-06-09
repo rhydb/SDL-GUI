9fc588a5bc24a615d35ee5439c1949d0f24cdc8f
nijabutter <rhydianbrown554@gmail.com>
2021-06-09 09:58:54 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,28 +1,38 @@
 #include <iostream>
-#include <SDL2/SDL.h>
-#undef main
-#include "Button.hpp"
-#include "Frame.hpp"
-#include "Label.hpp"
-#include "Entry.hpp"
-#include "Window.hpp"
+#include "SDL_GUI.hpp"
 
 int main() {
-    Window root = Window();
-    root.title("hello");
-    root.resizable(true);
-    root.background(150, 150, 150);
-    root.dimensions(500, 200);
+    Window* root = new Window();
+    (new Label(root, "Enter something:"))->grid(0, 0);
+    Entry* e = new Entry(root);
+    //e->set_placeholder("Placeholder text...");
+    e->set_w(300);
+    e->grid(0, 1);
 
-    Button* button = new Button(&root, "hello!");
-    button->grid(0, 0);
+    bool print_contents = true;
 
-    Window test = Window();
-    test.title("test");
-    Button* a = new Button(&test, "ermmm");
-    a->grid(0, 0);
-    test.run();
+    Button* b = new Button(root, "Click me", [e, x = &print_contents]() {
+        std::cout << "Clicked!";
+        if (*x) {
+            std::cout << "\tContents: '" << e->get() << "'";
+        }
+        std::cout << std::endl;
+    });
+    b->set_tooltip("Try clicking me!");
+    b->grid(1, 0);
 
-    root.run();
+    (new Checkbox(root, "Print contents", &print_contents))->grid(1, 1);
+    Slider* s = new Slider(root);
+    s->grid(2, 0);
+
+    (new Button(root, "get %", [s]() {
+        Window* w = new Window();
+        (new Label(w, "test"))->grid(0, 0);
+        w->run();
+    }))->grid(2, 1);
+
+    root->run();
+    delete root;
+
     return 0;
 }
