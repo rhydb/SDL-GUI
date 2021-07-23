83916409cf47f7f231eaf8e197f7b2db14f4fe24
nijabutter <rhydianbrown554@gmail.com>
2021-07-23 14:16:08 +0100

    Fix L strings

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 int main() {
     Window* root = new Window();
-    (new Label(root, "Enter something:"))->grid(0, 0);
+    (new Label(root, L"Enter something:"))->grid(0, 0);
     Entry* e = new Entry(root);
     e->set_placeholder(L"Placeholder text...");
     e->set_w(300);
@@ -11,22 +11,23 @@ int main() {
 
     bool print_contents = true;
 
-    Button* b = new Button(root, "Click me", [e, x = &print_contents]() {
+    Button* b = new Button(root, L"Click me", [e, x = &print_contents]() {
         std::wcout << L"Clicked!";
         if (*x) {
             std::wcout << L"\tContents: '" << e->get() << L"'";
         }
         std::wcout << std::endl;
-        });
-    b->set_tooltip("Try clicking me!");
+    });
+
+    b->set_tooltip(L"Try clicking me!");
     b->grid(1, 0);
 
     int test = 5;
-    (new Checkbox(root, "Print contents", &print_contents))->grid(1, 1);
+    (new Checkbox(root, L"Print contents", &print_contents))->grid(1, 1);
     SliderInt* s = new SliderInt(root, 0, 10, &test);
     s->grid(2, 0);
 
-    (new Button(root, "get %", [test = &test]() {
+    (new Button(root, L"get %", [test = &test]() {
         *test = 9;
     }))->grid(2, 1);
 
