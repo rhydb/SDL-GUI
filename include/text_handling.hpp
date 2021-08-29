0c05a9316eb13db38ce20c7f94c6c4ab0d35650b
nijabutter <rhydianbrown554@gmail.com>
2021-08-29 22:01:48 +0100

    Add text handling It attempts to help with multibyte

--- include/text_handling.hpp
+++ include/text_handling.hpp
@@ -1,4 +1,4 @@
 #pragma once
 #define IS_NOT_START_BYTE(x) (((x & 0b10000000) != 0) && ((x & 0b01000000) == 0))
-
+#define IS_MB_START_BYTE(x) ((x & 0b11000000) == 0b11000000)
 unsigned long long strgetmblen(const char* str);
